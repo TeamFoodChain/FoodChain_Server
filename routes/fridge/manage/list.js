dc3c8b3b641f4befd00fe54335a6cba9480344b1
이솔 <sol6252@naver.com>
2018-07-07 03:07:10 +0900

    fridge/manage/list
    해당 사용자의 냉장고 상품 목록 출력

--- routes/fridge/manage/list.js
+++ routes/fridge/manage/list.js
@@ -1,9 +1,44 @@
 const express = require('express');
 const router = express.Router();
+const db = require('../../../module/pool.js');			// queryParam_None, queryParam_Arr 두 개의 메소드
+const jwt = require('../../../module/jwt.js');
 
-router.get('/', (req, res, next) => {
-    res.render('index', { title: 'fridger/manage/list' });
+router.get('/', async (req, res)=>{
+    let token = req.headers.token;
+
+    if (!token) {
+		res.status(400).send({
+			message : "token is empty"
+		});
+	}else{
+        let decoded = jwt.verify(token);
+
+        checkQuery = "SELECT user_idx FROM user WHERE user_email = ?";
+        checkResult = await db.queryParam_Arr(checkQuery, [decoded.user_email]);
+        console.log(decoded.user_email);
+        console.log(checkResult);
+        
+        let user_idx = checkResult[0].user_idx;
+
+        if(!checkResult){
+            res.status(400).send({
+                message : "No user_idx value"
+            });
+        } else {
+            let getItemListQuery = "SELECT fridge_item.* FROM fridge JOIN fridge_item ON fridge.fri_item_idx = fridge_item.fri_item_idx WHERE fridge.user_idx = ? ORDER BY fri_regist_date DESC;"
+            let itemList = await db.queryParam_Arr(getItemListQuery, [user_idx]);
+            if (!itemList) {
+                res.status(500).send({
+                    message : "Internal Server Error"
+                });
+            }else{
+                res.status(200).send({
+                    message : "Success to get Data",
+                    pro_info : itemList
+                });
+            }
+        }
+    }
 });
-
 
 module.exports = router;
