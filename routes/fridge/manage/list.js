5933756e86d51f0e9ac947eb947619a5169c679f
이솔 <sol6252@naver.com>
2018-07-11 01:20:45 +0900

    fridege/manage/list, registet 최종
    ㄹㅇ 최ㅗ종

--- routes/fridge/manage/list.js
+++ routes/fridge/manage/list.js
@@ -1,21 +1,35 @@
 const express = require('express');
 const router = express.Router();
 const db = require('../../../module/pool.js');			// queryParam_None, queryParam_Arr 두 개의 메소드
-const jwt = require('../../../module/jwt.js');
+const identifier = require('../../../module/token_identifier.js');
 
 router.get('/', async (req, res)=>{
     let token = req.headers.token;
+    let user_email;
+
+    return new Promise((resolve, reject)=>{
+        identifier(token, function(err, result){
+            if(err) reject(err);
+            else resolve(result);
+        });
+    }).then(function(identify_data){
+        user_email = identify_data; 
+    }).catch(function(err){
+        res.status(500).send({
+            message : err
+        });
+        return ;
+        console.log(err);
+    });
 
     if (!token) {
 		res.status(400).send({
 			message : "token is empty"
 		});
 	}else{
-        let decoded = jwt.verify(token);
-
         checkQuery = "SELECT user_idx FROM user WHERE user_email = ?";
-        checkResult = await db.queryParam_Arr(checkQuery, [decoded.user_email]);
-        console.log(decoded.user_email);
+        checkResult = await db.queryParam_Arr(checkQuery, [user_email]);
+        console.log(user_email);
         console.log(checkResult);
         
         let user_idx = checkResult[0].user_idx;
--- routes/fridge/manage/register.js
+++ routes/fridge/manage/register.js
@@ -1,78 +1,160 @@
 const express = require('express');
 const router = express.Router();
 const db = require('../../../module/pool.js');			// queryParam_None, queryParam_Arr 두 개의 메소드
-const jwt = require('../../../module/jwt.js');
 const moment = require('moment');
+const jwt = require('../../../module/jwt.js');
+const s3 = require('../../../config/s3multer.js');
+const multer = require('multer');
+const upload = multer();
 
-router.post('/', async (req, res)=>{
-    let fri_img = req.body.fri_img;
+router.post('/', upload.array('fri_img'), async (req, res)=>{
+    let token = req.headers.token;
+
+	let fri_item_idx = req.body.fri_item_idx;
     let fri_cate = req.body.fri_cate;
     let fri_name = req.body.fri_name;
     let fri_ex_date = req.body.fri_ex_date;
     let fri_info = req.body.fri_info;
     let fri_regist_date = moment().format("YYYY-MM-DD HH:mm:ss");
-    let token = req.headers.token;
-    let checkQuery;
-    let checkResult;
-    
-    if( !fri_img || !fri_cate || !fri_name || !fri_ex_date || !fri_info || !token || !fri_regist_date){
+
+    let fri_img = [];
+
+    if(!fri_cate || !fri_name || !fri_ex_date || !fri_info || !token || !fri_regist_date){
         res.status(400).send({
             message : "Value Error : Fail from client"
         });
-    }else{
+        console.log(fri_cate, fri_name, fri_ex_date, fri_info, token, fri_regist_date);
+    }else{    
         let decoded = jwt.verify(token);
-        
-        checkQuery = "SELECT user_idx FROM user WHERE user_email = ?";
-        checkResult = await db.queryParam_Arr(checkQuery, [decoded.user_email]);
 
-        let user_idx = checkResult[0].user_idx;
-
-        if (!checkResult){
-            res.status(500).send({
-                message : "Internal server error"
-            });
-        }else if(decoded == -1){
-            res.status(500).send({
-                message : "token err"
-            });
-        }else{ 
-            let registerQuery = "INSERT INTO fridge_item (fri_img, fri_cate, fri_name, fri_ex_date, fri_info, fri_regist_date) VALUES(?, ?, ?, ?, ?, ?)";
-            let registerResult = await db.queryParam_Arr(registerQuery, [fri_img, fri_cate, fri_name, fri_ex_date, fri_info, fri_regist_date]);
-            
-            if (!registerResult){ 
-                res.status(500).send({
-                    message : "Register Error"
+        if(!fri_item_idx) {
+            if(decoded == -1){
+                res.status(400).send({
+                    message : "Value Error : Fail from client"
                 });
             }else{
-                let checkQuery = "SELECT fri_item_idx FROM fridge_item WHERE fri_img = ? && fri_cate = ? && fri_name = ? && fri_ex_date = ? && fri_info = ? && fri_regist_date = ?";
-                let checkResult = await db.queryParam_Arr(checkQuery, [fri_img, fri_cate, fri_name, fri_ex_date, fri_info, fri_regist_date]);
-
-                let fri_item_idx = checkResult[0].fri_item_idx;
-
-                if (!checkResult){
-                    res.status(500).send({
-                        message : "fri_item_idx load Error"
+                let selectQuery = "SELECT user_idx FROM user WHERE user_email= ?";
+                let selectResult = await db.queryParam_Arr(selectQuery, [decoded.id]);
+                let user_idx = selectResult[0].user_idx;
+                
+                if(!selectResult){
+                    res.status(400).send({
+                        message : "Value Error : Fail from client"
                     });
                 }else{
-                    let selectQuery = "SELECT fri_item_idx FROM fridge WHERE fri_item_idx = ?"
-                    let selectResult = await db.queryParam_Arr(selectQuery, [fri_item_idx]);
+                    if(req.files){ // 이미지 db, s3에 저장
+                        console.log(req.files);
+                        // multer-s3를 이용하지 않고, multer로 이미지를 가져오고, s3를 이용해서 s3에 이미지 등록
+                        for(let i = 0 ; i < req.files.length ; i++){
+                            fri_img[i] = 'https://foodchainimage.s3.ap-northeast-2.amazonaws.com/' + Date.now() + '.' + req.files[i].originalname.split('.').pop();
+                            s3.upload(req.files[i]);
+                        }
+                    }
 
-                    if (selectResult.length === 1) {
-                        res.status(400).send({
-                          message:"fri_item_idx is already exists"
+                    let registerQuery = "INSERT INTO fridge_item (fri_cate, fri_name, fri_ex_date, fri_info, fri_regist_date) VALUES(?, ?, ?, ?, ?)";
+                    let registerResult = await db.queryParam_Arr(registerQuery, [fri_cate, fri_name, fri_ex_date, fri_info, fri_regist_date]);
+                    
+                    if (!registerResult){ 
+                        res.status(500).send({
+                            message : "Register Error"
                         });
-                    } else {
-                        let registerQuery = "INSERT INTO fridge (user_idx, fri_item_idx) VALUES (?, ?)";
-                        let registerResult = await db.queryParam_Arr(registerQuery, [user_idx, fri_item_idx]);
-                        
-                        if (!registerResult){
-                            res.status(400).send({
+                    }else{
+                        let checkQuery = "SELECT LAST_INSERT_ID() as fri_item_idx";
+                        let checkResult = await db.queryParam_Arr(checkQuery);
+
+                        let fri_item_idx = checkResult[0].fri_item_idx;
+
+                        if (!checkResult){
+                            res.status(500).send({
                                 message : "fri_item_idx load Error"
                             });
                         }else{
-                            res.status(200).send({
-                                message : "Success to upload Data"
-                            });
+                            let registerQuery = "INSERT INTO fridge (user_idx, fri_item_idx) VALUES (?, ?)";
+                            let registerResult = await db.queryParam_Arr(registerQuery, [user_idx, fri_item_idx]);
+
+                            console.log(user_idx, fri_item_idx);
+                            
+                            if (!registerResult){
+                                res.status(400).send({
+                                    message : "fri_item_idx load Error 실패"
+                                });
+                            }else{
+                                let registerQuery = "INSERT INTO fridge_item_image (fri_item_idx, fri_img) VALUES (?, ?)";
+                                for(let i = 0 ; i < fri_img.length ; i++){
+                                    let registerResult = await db.queryParam_Arr(registerQuery, [fri_item_idx, fri_img[i]]);
+                                    if(!registerResult) {
+                                        res.status(500).send({
+                                            message : "Internal Server Error"
+                                        });
+                                    }else{
+                                        res.status(200).send({
+                                            message : "Success to upload Data"
+                                        });
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }else{                         
+            let UpdateFriItemQuery = "UPDATE fridge_item SET fri_cate = ?, fri_name = ?, fri_ex_date = ?, fri_info = ? WHERE fri_item_idx = ?";
+            let UpdateFriItemResult = await db.queryParam_Arr(UpdateFriItemQuery, [fri_cate, fri_name, fri_ex_date, fri_info, fri_item_idx]);
+            if(!UpdateFriItemResult){
+                res.status(500).send({
+                    message : "Internal server error"
+                });
+            }else{
+                let getFriItemImageQuery = "SELECT fri_img FROM fridge_item_image WHERE fri_item_idx = ?";	
+                let getFriItemImageResult = await db.queryParam_Arr(getFriItemImageQuery, [fri_item_idx]);
+                if(!getFriItemImageResult){
+                    res.status(500).send({
+                        message : "Internal server error"
+                    });
+                }else{
+                    if(getFriItemImageResult.length != 0){
+						for(let i = 0 ; i < getFriItemImageResult.length ; i++){
+							fri_img[i] = {};
+							fri_img[i].Key = getFriItemImageResult[i].fri_img.substring(55, getFriItemImageResult[i].fri_img.length);
+						}
+						console.log(fri_img);
+                    }
+                    let DeleteFriItemImageQuery = "DELETE FROM fridge_item_image WHERE fri_item_idx = ?";
+                    let DeleteFriItemImageResult = await db.queryParam_Arr(DeleteFriItemImageQuery, [fri_item_idx]);
+                    if(!DeleteFriItemImageResult){
+                        res.status(500).send({
+                            message : "Internal server error"
+                        });
+                    }else{
+                        if(fri_img.length == 0){
+                        } else{
+                            console.log("fri_img : ");
+                            console.log(fri_img);
+                            s3.delete(fri_img);
+
+                            if(req.files){ // s3에 저장
+                                fri_img = [];
+                                console.log(req.files);
+                                // multer-s3를 이용하지 않고, multer로 이미지를 가져오고, s3를 이용해서 s3에 이미지 등록
+                                for(let i = 0 ; i < req.files.length ; i++){
+                                    fri_img[i] = 'https://foodchainimage.s3.ap-northeast-2.amazonaws.com/' + Date.now() + '.' + req.files[i].originalname.split('.').pop();
+                                    s3.upload(req.files[i]);
+                                }
+                            }
+
+                            let insertProductImageQuery = "INSERT INTO fridge_item_image (fri_item_idx, fri_img) VALUES(?, ?)";
+                            for(let i = 0 ; i < fri_img.length ; i++){
+                            let insertProductImageResult = await db.queryParam_Arr(insertProductImageQuery, [fri_item_idx, fri_img[i]]);
+                                if(!insertProductImageResult) {
+                                    res.status(500).send({
+                                        message : "Internal Server Error"
+                                    });
+                                }else{
+                                    res.status(200).send({
+                                        message : "Success to modify Data"
+                                    });
+                                }
+                            }
                         }
                     }
                 }
@@ -82,78 +164,75 @@ router.post('/', async (req, res)=>{
 });
 
 router.delete('/', async (req, res)=>{
-    let fri_item_idx = req.body.fri_item_idx;
     let token = req.headers.token;
+    let fri_item_idx = req.body.fri_item_idx;
+
     let checkQuery;
     let checkResult;
+    let fri_img = [];
     
     if( !fri_item_idx || !token) {
         res.status(400).send({
             message : "Value Error - Fail from client"
         });
-    } else{
+    }else{    
         let decoded = jwt.verify(token);
-
-        checkQuery = "SELECT user_idx FROM user WHERE user_email = ?";
-        checkResult = await db.queryParam_Arr(checkQuery, [decoded.user_email]);
         
-        let user_idx = checkResult[0].user_idx;
-
-        if (!checkResult){
-            res.status(500).send({
-                message : "Internal server error"
-            });
-        }else if(decoded == -1){
-            res.status(500).send({
-                message : "token err"
-            });
-        }else{ 
-            let deleteQuery = "DELETE FROM fridge WHERE user_idx = ? && fri_item_idx = ?";
-            let deleteResult = await db.queryParam_Arr(deleteQuery, [user_idx, fri_item_idx]);
-            if (!deleteResult){ 
-                res.status(500).send({
-                    message : "Fridge Delete Error"
-                });
-            }else{
-                let deleteQuery = "DELETE FROM fridge_item WHERE fri_item_idx = ?";
-                let deleteResult = await db.queryParam_Arr(deleteQuery, [fri_item_idx]);
-                if (!deleteResult){ 
-                    res.status(500).send({
-                        message : "Fridge item Delete Error"
-                    });
-                } else{
-                    res.status(200).send({
-                        message : "Success to delete Fridge item"
-                    });
-                }
-            }
-        }
-    }
-});
-
-router.put('/', async (req, res)=>{
-    let fri_item_idx = req.body.fri_item_idx;
-    let fri_img = req.body.fri_img;
-    let fri_cate = req.body.fri_cate;
-    let fri_name = req.body.fri_name;
-    let fri_ex_date = req.body.fri_ex_date;
-    let fri_info = req.body.fri_info;
-    
-    if( !fri_img || !fri_cate || !fri_name || !fri_ex_date || !fri_info || !fri_item_idx){
-        res.status(400).send({
-            message : "Value Error - Fail from client"
-        });
-    }else{
-        let registerQuery = "UPDATE fridge_item SET fri_img = ?, fri_cate = ?, fri_name = ?, fri_ex_date = ?, fri_info = ? WHERE fri_item_idx = ?";
-        let registerResult = await db.queryParam_Arr(registerQuery, [fri_img, fri_cate, fri_name, fri_ex_date, fri_info, fri_item_idx]);
-        if (!registerResult){ 
-            res.status(500).send({
-                message : "Modify Error"
+        if(decoded == -1){
+            res.status(400).send({
+                message : "Value Error - Fail from client"
             });
         }else{
-            res.status(200).send({
-                message : "Success to modify Data"
-            });
+            checkQuery = "SELECT fri_img FROM fridge_item_image WHERE fri_item_idx = ?";
+            checkResult = await db.queryParam_Arr(checkQuery, [fri_item_idx]);
+
+            if(checkResult.length != 0){
+                for(let i = 0 ; i < checkResult.length ; i++){
+                    fri_img[i] = {};
+                    fri_img[i].Key = checkResult[i].fri_img.substring(55, checkResult[i].fri_img.length);
+                }
+                console.log(fri_img);
+            }
+
+            if (!checkResult){
+                res.status(400).send({
+                    message : "Null Value"
+                });
+            }else{ 
+                let deleteQuery = "DELETE FROM fridge WHERE user_idx = ? && fri_item_idx = ?";
+                let deleteResult = await db.queryParam_Arr(deleteQuery, [identify_data.idx, fri_item_idx]);
+                if (!deleteResult){ 
+                    res.status(500).send({
+                        message : "Fridge Delete Error"
+                    });
+                }else{
+                    let deleteQuery = "DELETE FROM fridge_item WHERE fri_item_idx = ?";
+                    let deleteResult = await db.queryParam_Arr(deleteQuery, [fri_item_idx]);
+                    if (!deleteResult){ 
+                        res.status(500).send({
+                            message : "Fridge item Delete Error"
+                        });
+                    }else{
+                        let deleteQuery = "DELETE FROM fridge_item_image WHERE fri_item_idx = ?";
+                        for(let i = 0 ; i < fri_img.length ; i++){
+                            let deleteResult = await db.queryParam_Arr(deleteQuery, [fri_item_idx]);
+                            if(!deleteResult) {
+                                res.status(500).send({
+                                    message : "Internal Server Error"
+                                });
+                            }else{
+                                if(fri_img.length == 0){
+                                }else{
+                                    s3.delete(fri_img);
+                                }
+                                res.status(200).send({
+                                    message : "Success to upload Data"
+                                });
+                            }
+                        }
+                    }
+                }
+            }
         }
     }
 });
