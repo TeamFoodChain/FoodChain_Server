947b685364ed800319bf3768b032d4974f35d7b0
이솔 <sol6252@naver.com>
2018-07-12 22:31:01 +0900

    mypage/profile 추가 ( + router 수정) @다시@
    mypage/profile 추가 ( + router 수정) @다시@

--- routes/mypage/index.js
+++ routes/mypage/index.js
@@ -1,19 +1,20 @@
 const express = require('express');
 const router = express.Router();
-
+ 
+const profileRouter = require('./profile');
 const bookmarkRouter = require('./bookmark');
 const basketRouter = require('./basket');
 const commentRouter = require('./comment');
 const sellRouter = require('./sell/index');
-
+ 
 router.get('/', (req, res, next) => {
   res.render('index', { title: 'mypage' });
 });
-
+ 
 router.use('/bookmark', bookmarkRouter);
+router.use('/profile', profileRouter);
 router.use('/basket', basketRouter);
 router.use('/comment', commentRouter);
 router.use('/sell', sellRouter);
-
-
+ 
 module.exports = router;
